5bb753dd04a03f119558f73bb6f7e410cd1f7a4e
Hao Zhang <u6523462@anu.edu.au>
2023-01-19 22:43:02 +1100

    Jan 19

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "multiply.h"
 
+#include <vector>
+#include<algorithm>
+#include <string>
+#include <deque>
+
+using namespace std;
 
 /// Class ---------------------------------------------------------------------------------------------------------
 
@@ -229,5 +235,89 @@ int main() {
 	std::cout << h1.age << h2.age<< std::endl;
 	h1.say();
 	human::say();
+
+	printf("--------------------------------------------------------------------------------- stl starts\n");
+
+
+	vector<int> v;
+	v.push_back(10); // insert numbers into vector
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(10);
+
+	// visit data in vector by iterator
+
+	vector<int>::iterator itBegin = v.begin(); // first elem
+	vector<int>::iterator itEnd = v.end(); // next elem to the last elem
+	while (itBegin != itEnd) {
+		cout << *itBegin << endl;
+		itBegin++;
+	}
+
+	// visit by for loop1
+	for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+		cout << *it << endl;
+	}
+
+	// visit by index
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+	printf("--------------- vector ends\n");
+
+
+	// construct string
+	string s1; // 1 
+
+	const char* str = "hello there~";
+	string s2(str); // 2
+	cout<<s2 <<endl;
+
+	string s3(s2); //3
+	cout << s3 << endl;
+
+	string s4(10, 'a'); //4
+	cout << s4 << endl; 
+
+	// concatenate
+	string s5 = s3 += s2;
+	cout << s5 << endl;
+	string s6 = s3.append(s2);
+	cout << s6 << endl;
+	// find
+	int pos = s2.find("el");
+	cout << pos << endl;
+	// replace
+	s5.replace(0,3,"111");
+	cout << s5 << endl;
+	// compare
+	cout << s1.compare(s2) << endl;
+	// visit and modify
+	cout << s2[0] << endl;
+	s2[0] = 'x';
+	cout << s2[0] << endl;
+	// insert
+	s2.insert(1, "1111");
+	cout << s2 << endl;
+	// erase
+	s2.erase(1, 3);
+	cout << s2 << endl;
+	// sub
+	string s7 = s2.substr(1,3);
+	cout << s7 << endl;
+	printf("--------------- string ends\n");
+
+
+	deque<int> d1; 
+	for (int i = 0; i < 10;i++)
+		d1.push_back(i);
+	
+	for (int i = 0; i < d1.size();i++)
+		cout << d1[i] << endl;
+
+
+
+
 
 }
